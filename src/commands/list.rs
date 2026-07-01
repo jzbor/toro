b324717e85e0eb6293591a1184c1b4afc00c0544
jzbor <jzbor@posteo.net>
2026-07-01 12:40:00 +0200

    fix: Add commands/list

---
+++ src/commands/list.rs
@@ -0,0 +1,38 @@
+use crate::commands::Command;
+use crate::error::ToroResult;
+use crate::filter::Filter;
+use crate::{exec, home, Config};
+
+#[derive(clap::Args, Debug)]
+pub struct ListCommand {
+    /// Show numbers for all entries
+    #[clap(short, long)]
+    numbered: bool,
+
+    /// Sort tasks from top to bottom
+    #[clap(long)]
+    top_to_bottom: bool,
+
+    #[clap(flatten)]
+    filter: Filter,
+
+    #[clap(flatten)]
+    config: Config,
+}
+
+impl Command for ListCommand {
+    fn exec(&self) -> ToroResult<()> {
+        let file = home::load_or_create_data_file()?;
+
+        if let Some(cmd) = &self.config.view.cal_command {
+            exec::exec("sh", ["-c", cmd])?
+        }
+
+        file.list(self.numbered, !self.top_to_bottom, self.config.columns, &self.config.view, Some(&self.filter));
+        Ok(())
+    }
+
+    fn config_mut(&mut self) -> &mut Config {
+        &mut self.config
+    }
+}
